89ccf068666ec7f00bac0ee26ab0243bde416096
Otaviano Pires <otavianopires@gmail.com>
2021-03-13 11:34:35 -0500

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,7 @@
+=== WP Front Login ===
+
+Customize you login, lost password, reset password, and profile forms
+
+== Description ==
+
+This is a plugin to implement custom login, lost password, reset password, and profile forms
---
+++ wp-front-login.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:       WP Front Login
+ * Plugin URI:        https://otavianopires.com
+ * Description:       Customize you login, lost password, reset password, and profile forms.
+ * Version:           0.1
+ * Author:            Otaviano Pires Amancio
+ * Author URI:        https://otavianopires.com
+ * Text Domain:       wpfl
+ * Domain Path:       /lang
+ */
+
+ if ( ! defined( 'ABSPATH' ) ) exit;

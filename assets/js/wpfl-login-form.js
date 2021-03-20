7e673a70c88512a33b32161b20dc7e91f8f0e472
Otaviano Pires <otavianopires@gmail.com>
2021-03-20 02:32:36 -0400

    Added ajax to process login

---
+++ assets/js/wpfl-login-form.js
@@ -0,0 +1,31 @@
+( function($, window) {
+
+    $( function() {
+        let wpflLoginForm = $('#wpfl-login-form');
+
+        wpflLoginForm.on('submit', function(event) {
+            event.preventDefault();
+
+            wpflLoginForm.find( '.wpfl-submit' ).prop( 'disabled', true );
+
+            let data = {
+                _ajax_nonce: wpfl_login_form_obj.nonce,
+                action: 'wpfl_login_form',
+                form_data: $( this ).serialize()
+            }
+            
+            $.post( wpfl_login_form_obj.ajax_url, data )
+            .done( function( response ) {
+                console.log( response );
+                if ( response.success == true ) {
+                    window.location.replace( response.data.redirect_url );
+                }
+            })
+            .always( function() {
+                wpflLoginForm.find( '.wpfl-submit' ).prop( 'disabled', false );
+            })
+
+        });
+    } );
+
+} )( jQuery, window );
--- includes/LoginForm.php
+++ includes/LoginForm.php
@@ -17,6 +17,9 @@ class LoginForm {
     {
         add_action( 'wp_enqueue_scripts', array( $this, 'enqueueStylesAndScripts') );
         add_shortcode( 'wpfl-login-form', array( $this, 'loginFormShortcode') );
+
+        add_action( 'wp_ajax_nopriv_wpfl_login_form', array( $this, 'processLogin' ) );
+        add_action( 'wp_ajax_wpfl_login_form', array( $this, 'processLogin' ) );
     }
 
     /**
@@ -24,7 +27,16 @@ class LoginForm {
      */
     public function enqueueStylesAndScripts()
     {
-        wp_register_style('wpfl-login-form', WPFL_URL . 'assets/css/wpfl-login-form.css', 'style', WPFL_VERSION);
+        wp_register_style( 'wpfl-login-form-style', WPFL_URL . 'assets/css/wpfl-login-form.css', 'style', WPFL_VERSION );
+
+        wp_register_script( 'wpfl-login-form-script', WPFL_URL . 'assets/js/wpfl-login-form.js', array( 'jquery' ), WPFL_VERSION, true );
+
+        wp_localize_script( 'wpfl-login-form-script', 'wpfl_login_form_obj',
+            array(
+                'ajax_url' => admin_url( 'admin-ajax.php' ),
+                'nonce' => wp_create_nonce( 'wpfl-login-form-nonce' )
+            )
+        );
     }
 
     /**
@@ -32,12 +44,68 @@ class LoginForm {
      */
     public function loginFormShortcode( $atts )
     {
-        wp_enqueue_style('wpfl-login-form');
+        wp_enqueue_style( 'wpfl-login-form-style' );
+        wp_enqueue_script( 'wpfl-login-form-script' );
 
         ob_start();
         include_once WPFL_PATH . 'templates/login-form.php';
         $output = ob_get_clean();
         return $output;
+    }
+
+    /**
+     * Process User Login 
+     */
+    public function processLogin()
+    {
+        check_ajax_referer('wpfl-login-form-nonce');
+        
+        $params = array();
+        parse_str( $_POST['form_data'], $params );
+
+        if ( isset( $params['wpfl-username'] ) && empty( $params['wpfl-username'] ) ) {
+            wp_send_json_error(
+                array(
+                    'message' => 'Missing username'
+                )
+            );
+        }
+
+        if ( isset($params['wpfl-password']) && empty( $params['wpfl-password'] ) ) {
+            wp_send_json_error(
+                array(
+                    'message' => 'Missing password'
+                )
+            );
+        }
+
+        $credentials = array(
+            'user_login'    => sanitize_user( $params['wpfl-username'] ),
+            'user_password' => $params['wpfl-password'],
+            'remember'      => isset( $params['wpfl-rememberme'] ) && $params['wpfl-rememberme'] == 'forever' ? true : false
+        );
+     
+        $user = wp_signon( $credentials, true );
+     
+        if ( is_wp_error( $user ) ) {
+            wp_send_json_error(
+                array(
+                    'message' => $user->get_error_message()
+                )
+            );
+        }
+
+        wp_clear_auth_cookie();
+        wp_set_current_user( $user->ID );
+        wp_set_auth_cookie( $user->ID );
+
+        wp_send_json_success(
+            array(
+                'message' => 'All good!',
+                'form_data' => $params,
+                'redirect_url' => home_url()
+            )
+        );
     }
 
 }

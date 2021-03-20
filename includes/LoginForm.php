f3d3f8d6f296d67fe17f00ac14197e11c4ec7783
Otaviano Pires <otavianopires@gmail.com>
2021-03-20 01:14:15 -0400

    Added CSS file and updated form

--- includes/LoginForm.php
+++ includes/LoginForm.php
@@ -15,7 +15,16 @@ class LoginForm {
      */
     private function init()
     {
+        add_action( 'wp_enqueue_scripts', array( $this, 'enqueueStylesAndScripts') );
         add_shortcode( 'wpfl-login-form', array( $this, 'loginFormShortcode') );
+    }
+
+    /**
+     * Enqueue Styles and Scripts
+     */
+    public function enqueueStylesAndScripts()
+    {
+        wp_register_style('wpfl-login-form', WPFL_URL . 'assets/css/wpfl-login-form.css', 'style', WPFL_VERSION);
     }
 
     /**
@@ -23,6 +32,8 @@ class LoginForm {
      */
     public function loginFormShortcode( $atts )
     {
+        wp_enqueue_style('wpfl-login-form');
+
         ob_start();
         include_once WPFL_PATH . 'templates/login-form.php';
         $output = ob_get_clean();
--- templates/login-form.php
+++ templates/login-form.php
@@ -6,14 +6,16 @@
             <input type="text" name="wpfl-username", id="wpfl-username" class="wpfl-input wpfl-input-username">
         </div>
         <div class="wpfl-from-group">
-            <label for="wpfl-password" class="wpfl-login-form-label"><?php _e('Username or Email Address', WPFL_TEXT_DOMAIN); ?></label>
+            <label for="wpfl-password" class="wpfl-login-form-label"><?php _e('Password', WPFL_TEXT_DOMAIN); ?></label>
             <input type="password" name="wpfl-password", id="wpfl-password" class="wpfl-input wpfl-input-password">
         </div>
-        <div class="wpfl-from-group">
-            <input type="checkbox" id="wpfl-rememberme" name="wpfl-rememberme" value="forever">
-            <label for="wpfl-rememberme"> Remember Me</label>
+        <div class="wpfl-from-group wpfl-from-group-submit">
+            <div class="wpfl-rememberme-container">
+                <input type="checkbox" id="wpfl-rememberme" name="wpfl-rememberme" value="forever">
+                <label for="wpfl-rememberme"> <?php _e('Remember Me', WPFL_TEXT_DOMAIN); ?></label>
+            </div>
             <button class="wpfl-submit"><?php _e('Login', WPFL_TEXT_DOMAIN); ?></button>
         </div>
     </form>
-    
+
 </div>

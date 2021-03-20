2bcf71033b34cd7f49ad88533a2dad9def5d6793
Otaviano Pires <otavianopires@gmail.com>
2021-03-20 11:50:12 -0400

    Prompt alert for messages after response

--- assets/js/wpfl-login-form.js
+++ assets/js/wpfl-login-form.js
@@ -2,11 +2,13 @@
 
     $( function() {
         let wpflLoginForm = $('#wpfl-login-form');
+        let wpflAlert = $('.wpfl-alert');
 
         wpflLoginForm.on('submit', function(event) {
             event.preventDefault();
 
             wpflLoginForm.find( '.wpfl-submit' ).prop( 'disabled', true );
+            wpflAlert.removeClass('wpfl-alert-error wpfl-alert-success').html('').hide();
 
             let data = {
                 _ajax_nonce: wpfl_login_form_obj.nonce,
@@ -17,9 +19,12 @@
             
             $.post( wpfl_login_form_obj.ajax_url, data )
             .done( function( response ) {
-                console.log( response );
                 if ( response.success == true ) {
                     window.location.replace( response.data.redirect_url );
+                    wpflAlert.addClass('wpfl-alert-success').html(response.data.message).show();
+                } else {
+                    console.log(response.data.message);
+                    wpflAlert.addClass('wpfl-alert-error').html(response.data.message).show();
                 }
             })
             .always( function() {

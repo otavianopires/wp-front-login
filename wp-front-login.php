34d3491005eb27b979ec3f0eac834f10be9ff360
Otaviano Pires <otavianopires@gmail.com>
2021-03-22 20:08:24 -0400

    Updated plugin description and version

--- wp-front-login.php
+++ wp-front-login.php
@@ -2,8 +2,8 @@
 /**
  * Plugin Name:       WP Front Login
  * Plugin URI:        https://github.com/otavianopires/wp-front-login
- * Description:       Customize you login, lost password, reset password, and profile forms.
- * Version:           0.1
+ * Description:       Customize you login and profile forms.
+ * Version:           0.2
  * Author:            Otaviano Pires Amancio
  * Author URI:        https://github.com/otavianopires
  * License:           GPL v2 or later
@@ -14,7 +14,7 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define( 'WPFL_VERSION', '0.1' );
+define( 'WPFL_VERSION', '0.2' );
 define( 'WPFL_PATH', plugin_dir_path(__FILE__) );
 define( 'WPFL_URL', plugin_dir_url(__FILE__) );
 define( 'WPFL_TEXT_DOMAIN', 'wpfl' );
